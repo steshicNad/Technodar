2c62aea852ba14b13e7c2dd986a154db85b36de0
Nadya <nadyasteshits@yandex.ru>
2023-01-17 22:45:41 +0300

    commit 3

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        myfont1:['EuclidCircularA', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+   
+  ],
+}
